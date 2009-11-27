48732210ec68c1e5f20db70d6871e0ac107a827d
Martin Friedrich <M.Friedrich@.(none)>
2009-11-27 10:02:56 +0100

    added remote cam storage

--- gadget/fit/CamWidget/camwidget_canvas.js
+++ gadget/fit/CamWidget/camwidget_canvas.js
@@ -9,14 +9,33 @@ function init() {
 
 
 // TODO: Use correct CAM Schema
+// TODO: unterscheide: local remote speichern
 function store(storagemode)
 {
-	var db = google.gears.factory.create('beta.database');
-	db.open('database-test');
-	db.execute('create table if not exists TestCam' + ' (Event text, Timestamp int)');
-	var date = new Date();
-	db.execute('insert into TestCam values (?, ?)', [storagemode, date.getTime()]); 
-	rs.close();	
+	if (storagemode == 'local') {
+		var db = google.gears.factory.create('beta.database');
+		db.open('database-test');
+		db.execute('create table if not exists TestCam' + ' (Event text, Timestamp int)');
+		var date = new Date();
+		db.execute('insert into TestCam values (?, ?)', [storagemode, date.getTime()]); 
+		rs.close();	
+	}
+	else if (storagemode == 'remote') {
+		var date = new Date();
+		var p1 = storagemode;
+		var p2 = new Date().getTime();
+		var params = 'p1=' + encodeURIComponent(p1) + '&p2=' + encodeURIComponent(p2); 
+		var url = 'http://duccio.informatik.rwth-aachen.de:9080/axis2/services/Main/storeCamToDB';
+		var xmlHttp = null;
+		try {
+		      // Mozilla, Opera, Safari sowie Internet Explorer (ab v7)
+		      xmlHttp = new XMLHttpRequest();
+		} catch(e) {
+			alert('request failed' + e);
+		}
+		xmlHttp.open("GET", url + '?' + params, true); 
+		xmlHttp.send(null);
+	}
 } 
 
 function query(limit)
